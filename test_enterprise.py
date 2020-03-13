f21befd446878309d341120f8be7c28da0f1404a
Namrah <nshahid@email.arizona.edu>
2020-03-13 14:16:00 +0000

    test class updated

--- test_enterprise.py
+++ test_enterprise.py
@@ -1,1 +1,48 @@
-we're going to space
+import unittest
+from enterprise import Enterprise
+import io
+import contextlib
+
+class TestEnterprise(unittest.TestCase)
+
+    def setUp(self):
+        self.enterprise_instance = Enterprise("test_name")
+
+    def test_max_power_level(self):
+        self.assertEqual(self.enterprise_instance.max_power_level, 1000)
+
+    def test_power_level(self):
+        self.assertEqual(self.enterprise_instance.power_level, 1000)
+
+    def test_max_population(self):
+        self.assertEqual(self.enterprise_instance.max_population, 30)
+
+    def test_population(self):
+        self.assertEqual(self.enterprise_instance.population, 0)
+
+    def test_max_shield_level(self):
+        self.assertEqual(self.enterprise_instance.max_shield_level, 100)
+
+    def test_shield_level(self):
+        self.assertEqual(self.enterprise_instance.shield_level, 100)
+
+    def test_str(self):
+        f = io.StringIO()
+        with contextlib.redirect.stdout(f):
+            self.enterprise_instance.str()
+
+        str_output = f.getvalue()
+
+        self.assertEqual(str_output, "Welcome to the Enterprise!\n")
+
+    def test_repr(self):
+        f = io.StringIO()
+        with contextlib.redirect.stdout(f):
+            self.enterprise_instance.str()
+
+        repr_output = f.getvalue()
+
+        self.assertEqual(repr_output, "NHLC Enterprise\n")
+
+if __name__ == '__main__':
+    unittest.main()

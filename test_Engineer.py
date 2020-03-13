30136fc220ed330fb5b8bffe526c66c4ddbc66f4
Louis Claxton <louis.m.claxton@gmail.com>
2020-03-13 16:28:19 +0000

    Tests passing base station

---
+++ test_Engineer.py
@@ -0,0 +1,28 @@
+import unittest
+from base_station import *
+import io
+import contextlib
+from typing import List
+
+
+class TestEngineerStation(unittest.TestCase):
+
+    def setUp(self) -> None:
+        self.Engineer_station_instance = EngineerStation('Engineer')
+
+    def test_name_sad(self):
+        with self.assertRaises(ValueError) as cm:
+            broken_name_instance = BaseStation(1)
+        self.assertEqual(str(cm.exception), 'Station name should be a string')
+
+    def test_str(self):
+        string_output = str(self.Engineer_station_instance)
+        self.assertEqual(string_output, "Welcome to the {}".format(self.Engineer_station_instance.name))
+
+    def test_repr(self):
+        repr_output = repr(self.Engineer_station_instance)
+        self.assertEqual(repr_output, "NHLC Enterprise Engineer station")
+
+
+if __name__ == '__main__':
+    unittest.main()
